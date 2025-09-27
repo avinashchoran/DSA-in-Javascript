d984f7f4cd6f812a3bc04b354f24c7281c38d373
Avinash Choran <avinashmanojanchoran@gmail.com>
2025-09-27 23:56:28 +0530

    Create Merge k Sorted Lists.js

---
+++ Patterns/Linked List/Merge k Sorted Lists.js
@@ -0,0 +1,101 @@
+// filepath: Patterns/Linked List/Merge k Sorted Lists.js
+/**
+ * Problem: Merge k Sorted Lists
+ * Difficulty: Hard
+ *
+ * You are given an array of k linked-lists, where each linked-list is sorted in ascending order.
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ *
+ * Constraints:
+ * - k == lists.length
+ * - 0 <= k <= 10^4
+ * - 0 <= lists[i].length <= 500
+ * - -10^4 <= lists[i][j] <= 10^4
+ * - lists[i] is sorted in ascending order.
+ * - The sum of lists[i].length will not exceed 10^4.
+ *
+ * Example 1:
+ * Input: lists = [[1,4,5],[1,3,4],[2,6]]
+ * Output: [1,1,2,3,4,4,5,6]
+ * Explanation: The linked-lists are:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * Merging them into one sorted list:
+ * 1->1->2->3->4->4->5->6
+ *
+ * Example 2:
+ * Input: lists = []
+ * Output: []
+ *
+ * Example 3:
+ * Input: lists = [[]]
+ * Output: []
+ */
+
+// LeetCode URL: https://leetcode.com/problems/merge-k-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    if (!lists || lists.length === 0) {
+        return null;
+    }
+
+    while (lists.length > 1) {
+        let mergedLists = [];
+        for (let i = 0; i < lists.length; i += 2) {
+            const l1 = lists[i];
+            const l2 = (i + 1) < lists.length ? lists[i + 1] : null;
+            mergedLists.push(mergeTwoLists(l1, l2));
+        }
+        lists = mergedLists;
+    }
+
+    return lists[0];
+};
+
+var mergeTwoLists = function(l1, l2) {
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    let head;
+    if (l1.val <= l2.val) {
+        head = l1;
+        l1 = l1.next;
+    } else {
+        head = l2;
+        l2 = l2.next;
+    }
+
+    let current = head;
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    if (l1) {
+        current.next = l1;
+    } else if (l2) {
+        current.next = l2;
+    }
+
+    return head;
+};
